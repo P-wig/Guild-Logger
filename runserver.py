0b2da95f2c10d8461533ee5fa377344dc8ae053b
Isaac Shepherd <isaacshepherdatwork@gmail.com>
2025-06-15 16:13:28 -0500

    starting new project from simple-flask template

--- runserver.py
+++ runserver.py
@@ -3,10 +3,12 @@
 # The program assumes that you have python3 and pip3 installed on your machine.
 # In the VSCode terminal window type the following:
 #    pip3 install -r requirements.txt
+
 # In VSCode, press Cmd-Shift-P (on Mac) or Ctrl-Shift-P (on Windows).
 #   In the command selection text box, type "Python Create"
 #   From the options dropdown select "Python Create Environment"
 #   From the next options dropdown select "venv"
+
 # Google Mail:  Use your current Google mail account or create a new one:
 #   Use this link to create an "App Password" that will allow this program to use the 
 #   Gmail acccount to send the email to the email address to the account specified
