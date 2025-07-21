2816db8d43375e8fc33f20c9b6d520a7fab1783a
Isaac Shepherd <isaacshepherdatwork@gmail.com>
2025-07-21 02:23:52 -0500

    minor modifications to page names

--- app/route_manager.py
+++ app/route_manager.py
@@ -18,7 +18,7 @@ from .routes.guild_route import guild_blueprint
 main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route('/')
-def home():
+def home_page():
   return render_template('home.html')
 
        
--- app/routes/guild_route.py
+++ app/routes/guild_route.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 guild_blueprint = Blueprint('guild', __name__, url_prefix='/guild')
 
 @guild_blueprint.route('/')
-def guild_home():
+def guild_page():
     return render_template('guild.html')
